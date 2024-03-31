216487fc3addcf3c3dd14190b286687ca2b84571
SaidLubawi <saidlubawi192@student.uir.ac.id>
2024-03-31 23:01:09 +0700

    Tugas PBK

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Said Lubawi<br>
+      <a href="###" target="_blank" rel="noopener">Home</a> 
+      <a href="###" target="_blank" rel="noopener">About</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -15px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,53 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Deskripsi</template>
+
+    saya lahir di Perawang pada tgl 13 septemeber 2003 dan biasanyaa di panggil Bawik
+    saya anak kedua dari dua bersaudara, tamatan dari SMA OLAHRAGA PROV RIAU.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+
+    sekarang saya menjalanin pendidikan S1 di ssalah satu universitas yaitu
+    <a href="###" target="_blank" rel="noopener">UNVERSITAS ISLAM RIAU</a>
+    Dan sekarang sudah meyelesaikan 3 semester.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Jurusan</template>
+    saya mengambil fakultas  
+    <a href="###" target="_blank" rel="noopener">Teknik</a>
+    Dan Prodi
+    <a href="###" target="_blank" rel="noopener">Teknik Informatika</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Hobby</template>
+    <a href="###" target="_blank" rel="noopener">Sepak Bola</a><br>
+    <a href="###" target="_blank" rel="noopener">Mokel</a><br>
+    <a href="###" target="_blank" rel="noopener">Ganggu bg Han</a><br>
+  </WelcomeItem>
+
+</template>
